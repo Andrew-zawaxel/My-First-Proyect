ce7b30816b5c61b64fd91f670388710aa911735b
Andrew-zawaxel <andrew.zambrano74868@ucaldas.edu.co>
2026-05-12 16:20:32 -0500

    feat. feo

---
+++ src/cosa.java
@@ -0,0 +1,7 @@
+public class cosa {
+    static String variable = "NIGGERS";
+    public static void main(String[] args) {
+        System.out.println("Otra cosa chistorra");
+        System.out.println(variable);
+    }
+}
