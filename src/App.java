5b77a67bec58fcd01295d5d90c45ae5f3b575916
Andrew-zawaxel <andrew.zambrano74868@ucaldas.edu.co>
2026-05-12 16:16:53 -0500

    feat. condicionales

--- src/App.java
+++ src/App.java
@@ -1,6 +1,20 @@
 public class App {
+
+    static int numero1 = 10;
+    static int numero2 = 20;
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Welcome NIGGERS!");
+        System.out.println("Numero 1: " + numero1);
+        System.out.println("Numero 2: " + numero2);
+
+        if (numero1 > numero2) {
+            System.out.println("Numero 1 es mayor que Numero 2");
+        } else {
+            System.out.println("Numero 2 es mayor o igual que Numero 1");
+        }
     }
 }
+
+
